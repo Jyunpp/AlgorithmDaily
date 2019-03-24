cac77496706cdce5ce48ba85fcf9d4a27c46ae15
Kim JiHyun <kcjohnnykim@gmail.com>
2019-03-25 00:19:19 +0900

    20190325 Codeforces 977A solved

---
+++ 2019-03/977A.cpp
@@ -0,0 +1,38 @@
+// 20190325 00:16 ~ 00:18
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int n, k;
+int main() {
+    cin>>n>>k;
+    rep(i, 1, k+1) {
+        if(n%10) n--;
+        else {
+            n/=10;
+        }
+    }
+    cout<<n<<endl;
+}
