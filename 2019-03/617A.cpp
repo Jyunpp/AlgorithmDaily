3ceb5bc6b506e1262d56637c18f7d3668b6f9017
Kim JiHyun <kcjohnnykim@gmail.com>
2019-03-30 21:04:40 +0900

    20190330 Codeforces 617A solved

---
+++ 2019-03/617A.cpp
@@ -0,0 +1,32 @@
+// 20190330 09:03 ~ 09:04
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int main() {
+    int n;cin>>n;
+    cout<<n/5 +((n%5)!=0)<<endl;
+    return 0;
+}
