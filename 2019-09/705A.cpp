5aa982f57c9d7e2491af5dfe4426a0f795595460
Kim JiHyun <kcjohnnykim@gmail.com>
2019-09-19 12:50:17 +0900

    20190919 705A solved

---
+++ 2019-09/705A.cpp
@@ -0,0 +1,36 @@
+// 20190919 12:43 ~ 12:48
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int n;
+int main() {
+    cin>>n;
+    cout<<"I hate ";
+    rep(i, 0, (n-1)/2) cout<<"that I love that I hate ";
+    if((n-1)&1) cout<<"that I love ";
+    cout<<"it"<<endl;
+    return 0;
+}
