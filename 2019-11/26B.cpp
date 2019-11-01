932431a22fed2c8f95d9235191715ab0ad368f4f
Kim JiHyun <kcjohnnykim@gmail.com>
2019-11-01 19:03:53 +0900

    20191101 26B solved, greedy

---
+++ 2019-11/26B.cpp
@@ -0,0 +1,42 @@
+// 20191101 18:24 ~ 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+ 
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+ 
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+ 
+string s;
+int curr, ans;
+
+int main() {
+    cin>>s;
+    rep(i, 0, s.length()) {
+        if(s[i] == '(') {
+            curr++, ans++;
+        } else if(curr > 0) {
+            curr--, ans++;
+        }
+    }
+    cout<<ans-curr<<endl;
+    return 0;
+}
