455251750ec543001fa81f31f62c47002fcd46a7
Kim JiHyun <kcjohnnykim@gmail.com>
2019-12-22 02:12:51 +0900

    20191222 33C solved

---
+++ 2019-12/33C.cpp
@@ -0,0 +1,42 @@
+// 20191222
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+using namespace std;
+ 
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+ 
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int n, s, mx, ts;
+int arr[100001];
+
+int main() {
+    cin>>n;
+    rep(i, 1, n+1) {
+        cin>>arr[i];
+        s += arr[i];
+        ts += arr[i];
+        if (ts < 0) ts = 0;
+        mx = max(mx, ts);
+    }
+    cout<< 2*mx - s <<endl;
+    return 0;
+}
