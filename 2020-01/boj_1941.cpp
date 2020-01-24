2abe091f5d7aa39930d70aaeb0fc9de7aed7bcc9
Kim JiHyun <kcjohnnykim@gmail.com>
2020-01-24 15:54:47 +0900

    20200124 BOJ_1941 removed unnecessary parameter

--- 2020-01/boj_1941.cpp
+++ 2020-01/boj_1941.cpp
@@ -30,8 +30,7 @@ int dy[4] = {1, -1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
 int arr[6][6];
 set<int> ans;
-map<P, int> visited;
-int debugArr[6][6];
+map<int, int> visited;
 
 int getKey(vector<P> &pv) {
 	int key = 0;
@@ -43,56 +42,39 @@ int getKey(vector<P> &pv) {
 	return key;
 }
 
-void recur(vector<P> &path, int y, int x) {
-	visited[{getKey(path), y*100+x}]++;
+void recur(vector<P> &path) {
+	visited[getKey(path)]++;
 	if(path.size() == 7) {
 		int sCnt = 0;
 		for (P p: path) sCnt += arr[p.xx][p.yy];
 
 		if (sCnt >= 4) {
-			// for DEBUG
-			// int prevSz = ans.size();
 			ans.insert(getKey(path));
-			// int nextSz = ans.size();
-			// if(prevSz != nextSz) {
-			// 	rep(i, 1, 6) rep(j, 1, 6) debugArr[i][j] = 0;
-			// 	for(P p : path) {
-			// 		debugArr[p.xx][p.yy] = 1;
-			// 		// cout<<"("<<p.xx<<", "<<p.yy<<"), ";
-			// 	}
-			// 	cout<<endl<<"answer.."<<getKey(path)<<endl;
-			// 	rep(i, 1, 6) {
-			// 		rep(j, 1, 6) cout<<debugArr[i][j];
-			// 		cout<<endl;
-			// 	}
-			// }
 		}
 		return ;
 	}
-
-	rep(i, 0, 4) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny <= 5 && nx <= 5 && nx*ny) {
-			bool isInPath = false;
-			for (P p: path) {
-				if (p.xx == ny && p.yy == nx) {
-					isInPath = true;
-					break;
+	for(P p : path) {
+		rep(i, 0, 4) {
+			int ny = p.xx + dy[i];
+			int nx = p.yy + dx[i];
+			if(ny <= 5 && nx <= 5 && nx*ny) {
+				bool isInPath = false;
+				for(P pIn: path) {
+					if (pIn.xx == ny && pIn.yy == nx) {
+						isInPath = true;
+						break;
+					}
 				}
-			}
-			if (!isInPath) {
-				vector<P> nextPath(path);
-				nextPath.pb({ny, nx});
-				for(P p : nextPath) {
-					if (visited[{getKey(nextPath), p.xx*100+p.yy}] == 0){
-						recur(nextPath, p.xx, p.yy);
+				if (!isInPath) {
+					vector<P> nextPath(path);
+					nextPath.pb({ny, nx});
+					if (visited[getKey(nextPath)] == 0){
+						recur(nextPath);
 					}
 				}
 			}
 		}
 	}
-
 }
 
 int main() {
@@ -108,7 +90,7 @@ int main() {
 		rep(j, 1, 6) {
 			vector<P> v;
 			v.pb({i, j});
-			recur(v, i, j);
+			recur(v);
 		}
 	}
 
