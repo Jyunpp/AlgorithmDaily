b606246409b2ec4f8441349ebe63a19202cfe770
Kim JiHyun <kcjohnnykim@gmail.com>
2020-08-19 23:33:05 +0900

    20200819 1374B solved

---
+++ 2020-08/1374B.cpp
@@ -0,0 +1,50 @@
+// 20200819 23:25 ~ 23:29
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <map>
+#include <set>
+using namespace std;
+
+#define sd(x) scanf(" %d", &x);
+#define slld(x) scanf(" %lld", &x);
+#define pb push_back
+#define xx first
+#define yy second
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define sortV(v, lambda) sort(v.begin(), v.end(), lambda)
+#define powll(a, b) (ll) pow(a, b)
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+
+int t, n;
+
+int main() {
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        
+        int twos = 0;
+        int threes = 0;
+        while(n%2==0) {
+            n/=2;
+            twos++;
+        }
+        while(n%3==0) {
+            n/=3;
+            threes++;
+        }
+        if(n!=1 || twos > threes) cout<<-1<<endl;
+        else cout<<2*threes-twos<<endl;
+    }
+    return 0;
+}
